56c6801620239aaa3047329b60c91567c87e4470
RasmusSoerensen <s214622@student.dtu.dk>
2025-09-13 11:11:27 +0200

    Resnet18
    Resnet18 tilføjet som class i networks.
    Der er også små ændringer som num_workers=7 og Image_size = 224 (nødvendigt for Resnet)

--- networks.py
+++ networks.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torch import Tensor
+from torchvision.models import resnet18, ResNet18_Weights
 
 class BaseClassifier(nn.Module):
     """
@@ -15,3 +16,15 @@ class BaseClassifier(nn.Module):
         x = x.view(x.size(0), -1)
         out = self.linear(x) # don't apply sigmoid here, use BCEWithLogitsLoss
         return out
+
+
+
+class ResNet18Binary(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = resnet18(weights=ResNet18_Weights.DEFAULT)
+        num_ftrs = self.model.fc.in_features #Number of features in the last layer
+        self.model.fc = nn.Linear(num_ftrs, 1) #Ensures that we have a single output and not 1000
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.model(x)  
