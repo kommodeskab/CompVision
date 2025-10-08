2c08f7de3d1657cb593f0e63466545ba20ee3e5c
NikolajT84 <nikolaj.topsoe@gmail.com>
2025-10-08 16:00:20 +0200

    Early fusion by input channels in resnet

--- networks.py
+++ networks.py
@@ -21,10 +21,12 @@ class ResNet18Binary(nn.Module):
     def __init__(
         self, 
         num_classes: int = 2,
+        in_channels: int = 3,
         hidden_size: int = 512,
         ):
         super().__init__()
         self.model = resnet18(weights=ResNet18_Weights.DEFAULT)
+        self.model.conv1.in_channels = in_channels
         num_ftrs = self.model.fc.in_features #Number of features in the last layer
         out_features = 1 if num_classes == 2 else num_classes
         self.model.fc = nn.Sequential(
